339348005980d69498ec992adc2012d70b7cc3ec
toshihirokato <nanamin.104@icloud.com>
2020-03-13 16:00:11 +0900

    画像、アイコンの表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <div class="container">hello</div>
+  <div class="">
+    <img src="~/assets/Rectangle1.png" alt="">
+    <img src="~/assets/Rectangle2.png" alt="">
+    <img src="~/assets/Rectangle3.png" alt="">
+    <img src="~/assets/Rectangle4.png" alt="">
+    <img src="~/assets/Rectangle5.png" alt="">
+    <img src="~/assets/Rectangle6.png" alt="">
+    <img src="~/assets/Rectangle7.png" alt="">
+    <img src="~/assets/Rectangle8.png" alt="">
+    <img src="~/assets/Rectangle9.png" alt="">
+    <img src="~/assets/Icon1.png" alt="">
+    <img src="~/assets/Icon2.png" alt="">
+    <img src="~/assets/Icon3.png" alt="">
+    <img src="~/assets/Icon4.png" alt="">
+    <img src="~/assets/Icon5.png" alt="">
+    <img src="~/assets/Icon6.png" alt="">
+    <img src="~/assets/Icon7.png" alt="">
+    <img src="~/assets/Rectangle10.png" alt="">
+    <img src="~/assets/Rectangle11.png" alt="">
+    <img src="~/assets/Rectangle12.png" alt="">
+    <img src="~/assets/Icon8.png" alt="">
+    <img src="~/assets/Rectangle13.png" alt="">
+    <img src="~/assets/Rectangle14.png" alt="">
+    <img src="~/assets/Icon9.png" alt="">
+    <img src="~/assets/Icon10.png" alt="">
+    <img src="~/assets/Icon11.png" alt="">
+    <img src="~/assets/Icon12.png" alt="">
+    <img src="~/assets/Icon13.png" alt="">
+    <img src="~/assets/Icon14.png" alt="">
+    <img src="~/assets/Icon15.png" alt="">
+  </div>
 </template>
