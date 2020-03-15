83b3215669b122ca78d50687dc0315736447aff4
toshihirokato <nanamin.104@icloud.com>
2020-03-16 04:12:11 +0900

    ヘッダーとメインの表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,422 @@
 <template>
-  <div class="">
-    <img src="~/assets/Rectangle1.png" alt="">
-    <img src="~/assets/Rectangle2.png" alt="">
-    <img src="~/assets/Rectangle3.png" alt="">
-    <img src="~/assets/Rectangle4.png" alt="">
-    <img src="~/assets/Rectangle5.png" alt="">
-    <img src="~/assets/Rectangle6.png" alt="">
-    <img src="~/assets/Rectangle7.png" alt="">
-    <img src="~/assets/Rectangle8.png" alt="">
-    <img src="~/assets/Rectangle9.png" alt="">
-    <img src="~/assets/Icon1.png" alt="">
-    <img src="~/assets/Icon2.png" alt="">
-    <img src="~/assets/Icon3.png" alt="">
-    <img src="~/assets/Icon4.png" alt="">
-    <img src="~/assets/Icon5.png" alt="">
-    <img src="~/assets/Icon6.png" alt="">
-    <img src="~/assets/Icon7.png" alt="">
-    <img src="~/assets/Rectangle10.png" alt="">
-    <img src="~/assets/Rectangle11.png" alt="">
-    <img src="~/assets/Rectangle12.png" alt="">
-    <img src="~/assets/Icon8.png" alt="">
-    <img src="~/assets/Rectangle13.png" alt="">
-    <img src="~/assets/Rectangle14.png" alt="">
-    <img src="~/assets/Icon9.png" alt="">
-    <img src="~/assets/Icon10.png" alt="">
-    <img src="~/assets/Icon11.png" alt="">
-    <img src="~/assets/Icon12.png" alt="">
-    <img src="~/assets/Icon13.png" alt="">
-    <img src="~/assets/Icon14.png" alt="">
-    <img src="~/assets/Icon15.png" alt="">
+  <div class="header">
+    <div class="header1">
+      <img src="~/assets/Rectangle1.png" alt="">
+    </div>
+    <div class="header2">
+      <img
+        src="~/assets/Icon12.png"
+        alt=""
+      >
+      <div class="title1">
+        CRYPTO
+      </div>
+      <div class="title2">
+        Coin
+      </div>
+      <div class="header-btn1">
+        ABOUT
+      </div>
+      <div class="header-btn2">
+        WHITE PAPER
+      </div>
+      <div class="header-btn3">
+        <img src="~/assets/Rectangle15.png" alt="">
+        <p>DOWNLOAD</p>
+      </div>
+    </div>
+    <div class="img2">
+      <img src="~/assets/Rectangle2.png" alt="">
+    </div>
+    <div class="text1">
+      CRYPTO
+    </div>
+    <div class="text2">
+      Coin
+    </div>
+    <div class="text3">
+      Secure wallet transfers,
+    </div>
+    <div class="text4">
+      today
+    </div>
+    <div class="img3">
+      <img src="~/assets/Rectangle16.png" alt="">
+    </div>
+    <div class="text5">
+      Sign Up Now
+    </div>
+    <div class="main-img">
+      <img src="~/assets/Rectangle17.png" alt="">
+    </div>
+    <div class="img4">
+      <img src="~/assets/Rectangle3.png" alt="">
+    </div>
+    <div class="img5">
+      <img src="~/assets/Rectangle4.png" alt="">
+    </div>
+    <div class="img6">
+      <img src="~/assets/Rectangle5.png" alt="">
+    </div>
+    <div class="text6">
+      Smooth Sailing
+    </div>
+    <div class="text7">
+      Decentralized
+    </div>
+    <div class="text8">
+      Web3 Injection
+    </div>
+    <div class="text9">
+      A wallet crafted to reduce the steep learning curve of the blockchain.
+    </div>
+    <div class="text10">
+      We believe in the importance of a decentralized future, that’s why we want to bring the blockchain to the 99%.
+    </div>
+    <div class="text11">
+      Cutting edge tech, that makes interacting with the Ethereum blockchain seamless.
+    </div>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.header1 {
+  position: absolute;
+  width: 1200px;
+  height: 565px;
+  left: 0px;
+  top: 0px;
+  border-radius: 0px;
+}
+
+.header2 img {
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  left: 48px;
+  top: 40px;
+  border-radius: 0px;
+}
+
+.title1 {
+  position: absolute;
+  width: 148px;
+  height: 40px;
+  left: 108px;
+  top: 41px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 34px;
+  line-height: 40px;
+  letter-spacing: 0.25px;
+  color: #FFFFFF;
+}
+
+.title2 {
+  position: absolute;
+  width: 69px;
+  height: 40px;
+  left: 256px;
+  top: 41px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 33px;
+  line-height: 40px;
+  letter-spacing: 0.25px;
+  color: #FFFFFF;
+}
+
+.header-btn1 {
+  position: absolute;
+  width: 59px;
+  height: 19px;
+  left: 925px;
+  top: 55px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 1.42857px;
+  text-transform: uppercase;
+  color: #FFFFFF;
+}
+
+.header-btn2 {
+  position: absolute;
+  width: 118px;
+  height: 19px;
+  left: 1044px;
+  top: 55px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 1.42857px;
+  text-transform: uppercase;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.header-btn3 img {
+  position: absolute;
+  width: 175px;
+  height: 64px;
+  left: 1197px;
+  top: 32px;
+  background: #3F51B5;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+
+.header-btn3 p {
+  position: absolute;
+  width: 82px;
+  height: 19px;
+  left: 1244px;
+  top: 55px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  letter-spacing: 1.42857px;
+  text-transform: capitalize;
+  color: #FFFFFF;
+}
+
+.img2 {
+  position: absolute;
+  width: 300px;
+  height: 600px;
+  left: 550px;
+  top: 172px;
+  border-radius: 0px;
+  filter: drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.6));
+}
+
+.text1  {
+  position: absolute;
+  width: 381px;
+  height: 96px;
+  left: 417px;
+  top: 788px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 96px;
+  line-height: 96px;
+  letter-spacing: 1.5px;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text2 {
+  position: absolute;
+  width: 185px;
+  height: 96px;
+  left: 798px;
+  top: 788px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 89px;
+  line-height: 96px;
+  letter-spacing: 1.5px;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text3 {
+  position: absolute;
+  width: 370px;
+  height: 40px;
+  left: 417px;
+  top: 892px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 34px;
+  line-height: 40px;
+  letter-spacing: 0.25px;
+  color: #9E9E9E;
+}
+
+.text4 {
+  position: absolute;
+  width: 84px;
+  height: 40px;
+  left: 788px;
+  top: 892px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 33px;
+  line-height: 40px;
+  letter-spacing: 0.25px;
+  color: #9E9E9E;
+}
+
+.img3 {
+  position: absolute;
+  width: 566px;
+  height: 64px;
+  left: 417px;
+  top: 980px;
+  background: #3F51B5;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+}
+
+.text5 {
+  position: absolute;
+  width: 116px;
+  height: 22px;
+  left: 643px;
+  top: 1000px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 19px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: 0.25px;
+  text-transform: capitalize;
+  color: #FFFFFF;
+}
+
+.main-img {
+  position: absolute;
+  width: 876px;
+  height: 504px;
+  left: 262px;
+  top: 1116px;
+  background: #FFFFFF;
+  border-radius: 17px;
+}
+
+.img4 {
+  position: absolute;
+  width: 244px;
+  height: 185px;
+  left: 310px;
+  top: 1176px;
+  border-radius: 0px;
+}
+
+.img5 {
+  position: absolute;
+  width: 244px;
+  height: 200px;
+  left: 578px;
+  top: 1176px;
+  border-radius: 0px;
+}
+
+.img6 {
+  position: absolute;
+  width: 244px;
+  height: 188px;
+  left: 846px;
+  top: 1176px;
+  border-radius: 0px;
+}
+
+.text6 {
+  position: absolute;
+  width: 164px;
+  height: 32px;
+  left: 350px;
+  top: 1393px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 32px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text7 {
+  position: absolute;
+  width: 149px;
+  height: 32px;
+  left: 625px;
+  top: 1393px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 32px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text8 {
+  position: absolute;
+  width: 149px;
+  height: 32px;
+  left: 887px;
+  top: 1393px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 32px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.87);
+}
+
+.text9 {
+  position: absolute;
+  width: 220px;
+  height: 72px;
+  left: 322px;
+  top: 1466px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.5px;
+  color: #9E9E9E;
+}
+
+.text10 {
+  position: absolute;
+  width: 237px;
+  height: 96px;
+  left: 582px;
+  top: 1466px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.5px;
+  color: #9E9E9E;
+}
+
+.text11 {
+  position: absolute;
+  width: 224px;
+  height: 72px;
+  left: 856px;
+  top: 1466px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.5px;
+color: #9E9E9E;
+}
+</style>
